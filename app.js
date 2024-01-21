6ea39eb8010eeb7548f53e5aa31dda5dff5c76ec
Saad Khan <155878793+saadyaseen12@users.noreply.github.com>
2024-01-21 11:31:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var length = 10;
+var width = 5;
+var area = length * width;
+console.log("The area of the rectangle is " + area); 
+
+
+
+var username = prompt("What is your name?");
+alert("Hello, " + username + "! Nice to meet you." );
